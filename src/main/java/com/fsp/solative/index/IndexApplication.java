38aed678ffe9b5df10d31aae813d516468e32cc7
hamid <sahlolbey@gmail.com>
2020-03-17 17:21:55 +0330

    moved

--- src/main/java/com/fsp/solative/index/IndexApplication.java
+++ src/main/java/com/fsp/solative/index/IndexApplication.java
@@ -1,4 +1,4 @@
-package com.fsp.solative.index.general;
+package com.fsp.solative.index;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
